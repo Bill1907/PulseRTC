7c8b5c4c35e42e8aa8ad91d5279dd742cc98184e
Bill1907 <a31829660@gamil.com>
2025-04-16 15:36:53 +0900

    .

--- packages/ai-hook/app/__init__.py
+++
@@ -1,3 +0,0 @@
-"""PulseRTC AI Hook service."""
-
-__version__ = "0.1.0" 
--- packages/sfu/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { SFU } from "./sfu";
-import * as types from "./types";
-import { defaultConfig } from "./config/default";
-import { AIHookConnector } from "./ai-hook";
-
-export { SFU, types, defaultConfig, AIHookConnector };
